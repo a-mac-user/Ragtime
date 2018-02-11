5338bf0124852b0db1e42377e7e07d4e5cf9ff76
a-mac-user <qq_1943@126.com>
2018-02-11 13:04:19 +0800

    更改文件结构

--- Ragtime/asset/urls.py
+++
@@ -1,8 +0,0 @@
-from asset import views
-from django.conf.urls import url
-
-urlpatterns = [
-    url(r'report/asset_with_no_asset_id/$', views.asset_with_no_asset_id),
-    url(r'new_assets/approval/$', views.new_assets_approval, name='new_assets_approval'),
-    url(r'^asset_list/(\d+)/$', views.asset_detail, name="asset_detail"),
-]
